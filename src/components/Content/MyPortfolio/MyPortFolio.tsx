593719342c07a94edec5cbcff5a55f0c0a50024b
LetsGoMan <ilikeseik@gmail.com>
2023-07-25 00:02:08 +0300

    add new projects images

--- src/components/Content/MyPortfolio/MyPortFolio.tsx
+++ src/components/Content/MyPortfolio/MyPortFolio.tsx
@@ -3,15 +3,25 @@ import ownStyles from "./MyPortfolio.module.css";
 import {Title} from "../../common/Title/Title";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faSuitcase} from "@fortawesome/free-solid-svg-icons";
-import project1 from "../../../assets/img/project-1.png"
-import project2 from "../../../assets/img/project-2.png"
-import project3 from "../../../assets/img/project-3.png"
-import project4 from "../../../assets/img/project-4.png"
-import project5 from "../../../assets/img/project-5.png"
-import project6 from "../../../assets/img/project-6.png"
-import project7 from "../../../assets/img/project-7.png"
-import project8 from "../../../assets/img/project-8.png"
-import project9 from "../../../assets/img/project-9.png"
+// import project1 from "../../../assets/img/project-1.png"
+// import project2 from "../../../assets/img/project-2.png"
+// import project3 from "../../../assets/img/project-3.png"
+// import project4 from "../../../assets/img/project-4.png"
+// import project5 from "../../../assets/img/project-5.png"
+// import project6 from "../../../assets/img/project-6.png"
+// import project7 from "../../../assets/img/project-7.png"
+// import project8 from "../../../assets/img/project-8.png"
+// import project9 from "../../../assets/img/project-9.png"
+
+import project1 from "../../../assets/img/project-1.jpg"
+import project2 from "../../../assets/img/project-2.jpg"
+import project3 from "../../../assets/img/project-3.jpg"
+import project4 from "../../../assets/img/project-4.jpg"
+import project5 from "../../../assets/img/project-5.jpg"
+import project6 from "../../../assets/img/project-6.jpg"
+import project7 from "../../../assets/img/project-7.jpg"
+import project8 from "../../../assets/img/project-8.jpg"
+import project9 from "../../../assets/img/project-9.jpg"
 import {Project} from "./Project/Project";
 import {GoBack} from "../../common/GoBack/GoBack";
 import {useNavigate} from "react-router-dom";
